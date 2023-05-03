6c32de38351571ca3ab5bd219e81970f3104071f
Sivaa0206 <vinsmokesanji020620@gmail.com>
2023-05-03 21:55:54 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
+import { GitHubService } from './github.service';
+import { repos } from './repos';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'get';
+  Username:string="Sivaa0206"
+  userName: string = "tektutorialshub"
+  repos: repos[] | any;
+ 
+  loading: boolean = false;
+  errorMessage:any;
+ 
+  constructor(private githubService: GitHubService) {
+  }
+ 
+  public getRepos() {
+    this.loading = true;
+    this.errorMessage = "";
+    this.githubService.getRepos(this.userName)
+      .subscribe(
+        (response) => {                           //next() callback
+          console.log('response received')
+          this.repos = response; 
+        },
+        (error) => {                              //error() callback
+          console.error('Request failed with error')
+          this.errorMessage = error;
+          this.loading = false;
+        },
+        () => {                                   //complete() callback
+          console.error('Request completed')      //This is actually not needed 
+          this.loading = false; 
+        })
+  }
 }
